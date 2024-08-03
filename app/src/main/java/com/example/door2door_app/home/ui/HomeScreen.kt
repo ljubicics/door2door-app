33de55e36c1913f54e0289300895c6cbfce7bc01
ljubicics <119794666+ljubicics@users.noreply.github.com>
2024-08-03 13:50:29 +0200

    Finish auth feature
    * Finish auth feature with login and register
    
    ---------
    
    Co-authored-by: Strahinja Ljubicic <sljubicic7120rn@raf.rs>

---
+++ app/src/main/java/com/example/door2door_app/home/ui/HomeScreen.kt
@@ -0,0 +1,17 @@
+package com.example.door2door_app.home.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().background(Color.White)
+    ) {
+
+    }
+}
