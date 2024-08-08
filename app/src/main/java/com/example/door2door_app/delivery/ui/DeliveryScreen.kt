43a3c29aaf3a4d2c1830706fa318445857886138
ljubicics <119794666+ljubicics@users.noreply.github.com>
2024-08-08 09:13:52 +0200

    Feature/role based navigation
    * Implement maps default api key gitignore
    
    * Add basic bottom navigation
    
    * Implement preference datastore
    
    * Add role based navigation
    
    * Optimize splash screen role logic

---
+++ app/src/main/java/com/example/door2door_app/delivery/ui/DeliveryScreen.kt
@@ -0,0 +1,23 @@
+package com.example.door2door_app.delivery.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DeliveryScreen(
+    modifier: Modifier = Modifier,
+    text: String = ""
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = text)
+    }
+}
