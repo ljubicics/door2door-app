0ffc2dff268bce33f41704e1f2e8482af744f7ad
ljubicics <119794666+ljubicics@users.noreply.github.com>
2024-08-16 22:33:02 +0200

    Feature/customer deliveries
    * Add deliveries screen
    
    * Finish customer deliveries screen

--- app/src/main/java/com/example/door2door_app/networking/factory/HttpClientFactory.kt
+++ app/src/main/java/com/example/door2door_app/networking/factory/HttpClientFactory.kt
@@ -43,7 +43,7 @@ object HttpClientFactory : KoinComponent {
                 contentType(ContentType.Application.Json)
                 url {
                     protocol = URLProtocol.HTTP
-                    host = "10.0.1.49"
+                    host = "192.168.1.146"
                     port = 8080
                     path("api/")
                 }
